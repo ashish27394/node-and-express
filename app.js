51e7c8448607644e4408d84a0015fab3dcf844de
Ashish Panjwani <ashish27394@gmail.com>
2018-10-28 13:07:48 +0530

    installing bootstrap and putting into css and js folder for direct access

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var app = express();
 
 //app.use(morgan('combined'));
 app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
+
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
